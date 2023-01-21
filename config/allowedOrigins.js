95d11d9efe184c7ae23ea1a84946b8787621267f
Jomel Bautista <jomelgbautista@gmail.com>
2023-01-21 13:54:46 -0800

    deploy config updates

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  // 'https://www.jomelb.com',
-  // 'https://jomelb.com'
+  // "http://localhost:3000",
+  'https://www.healthability-client-portal.com',
+  'https://healthability-client-portal.com',
+  'https://www.jomelb.com',
+  'https://jomelb.com'
 ];
 
 module.exports = allowedOrigins;
